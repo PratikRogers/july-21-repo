782f74aab14c1738e4d8bfa8acb6e8c206211cc9
PratikRogers <105482237+PratikRogers@users.noreply.github.com>
2022-08-08 06:24:30 -0400

    Update registerServiceWorker.ts

--- src/registerServiceWorker.ts
+++ src/registerServiceWorker.ts
@@ -47,7 +47,7 @@ export default function register() {
         });
       } else {
         // Is not local host. Just register service worker
-        registerValidSW('./service-worker.js');
+        registerValidSW(swUrl);
       }
     });
   }
