5b49a82712e471ef139e74701293ec5bd921e90c
PratikRogers <105482237+PratikRogers@users.noreply.github.com>
2022-08-08 08:55:25 -0400

    Update registerServiceWorker.ts

--- src/registerServiceWorker.ts
+++ src/registerServiceWorker.ts
@@ -32,7 +32,7 @@ export default function register() {
 
     window.addEventListener('load', () => {
       console.log('process.env.PUBLIC_URL===',process.env.PUBLIC_URL);
-      const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
+      const swUrl = './service-worker.js';
 
       if (isLocalhost) {
         // This is running on localhost. Lets check if a service worker still exists or not.
